d47c094c07ab8a36d8985cf6681ffb4af68309e1
LEN4 <fooggreitan@gmail.com>
2022-10-23 23:04:03 +0300

    1_new

--- EX3/Program.cs
+++ EX3/Program.cs
@@ -1,48 +1,35 @@
-﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-double[] CreateArrayRndDouble(int size, double min, double max)
+int[] CreateArrayRndInt(int size, int min, int max)
 {
 
-    var array = new double[size];
+    var array = new int[size];
     var rnd = new Random();
-
     for (int i = 0; i < size; i++)
     {
-        double rmd = rnd.NextDouble()* (max - min) + min;
-        array[i] = Math.Round(rmd, 1, MidpointRounding.ToZero);
+        array[i] = rnd.Next(min, max + 1);
     }
     return array;
 }
 
-double DubleRaz(double[] array)
+void PrintArray(int[] array)
 {
-    double maxFillarray = array[0];
-    double minFillarray = array[0];
-
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] > maxFillarray) maxFillarray = array[i];
-        else if (array[i] < minFillarray) minFillarray = array[i];
+        if (i < array.Length - 1) Console.Write($"{array[i]}, "); 
+        else Console.Write($"{array[i]} -> " );
     }
 
-    return maxFillarray - minFillarray;
-}
-
-void PrintArrayDouble(double[] array)
-{
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i < array.Length - 1) Console.Write($"{array[i]}; "); 
+        if (i < array.Length - 1) Console.Write($"{array[i]}, "); 
         else Console.Write($"{array[i]}");
     }
-    Console.WriteLine("]");
+    Console.Write("]");
 }   
 
-double[] arr = CreateArrayRndDouble(4, 3, 7); 
-PrintArrayDouble(arr);
-
-double des = Math.Round(DubleRaz(arr), 1, MidpointRounding.ToZero);
-Console.WriteLine($"Максимальное значение массива -> {des}");
+int[] arr = CreateArrayRndInt(8, 10, 100);
+PrintArray(arr);
