6a9bcb779f670c83056cdb48c00c20e645ebc47e
LEN4 <fooggreitan@gmail.com>
2022-10-23 23:03:54 +0300

    1_new

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,47 +1,35 @@
-﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
-// [3, 7, 23, 12] -> 19
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-// [-4, -6, 89, 6] -> 0
-
-int[] CreateArrayRndInt(int size, int min, int max)
+int AmountNumbers(int num)
 {
-
-    int[] array = new int[size];
-    var rnd = new Random();
-    for (int i = 0; i < size; i++)
+    int index = 0;
+    while (num > 0)
     {
-        array[i] = rnd.Next(min, max + 1);
+        num /= 10; 
+        index++; 
     }
-    return array;
+    return index;
 }
 
-int Sum(int[] array)
+int Sum(int num, int size)
 {
     int sum = 0;
-    
-    for (int i = 0; i <= array.Length; i++)
+
+    for (int i = 1; i <= size; i++)
     {
-        if(i % 2 == 1) sum += array[i];
+        sum += num % 10;
+        num /= 10;
     }
-    
+
     return sum;
 }
 
-void PrintArray(int[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i < array.Length - 1) Console.Write($"{array[i]}, "); 
-        else Console.Write($"{array[i]}");
-    }
-    Console.WriteLine("]");
-}   
-
-int[] arr = CreateArrayRndInt(4, -100, 100); 
-PrintArray(arr);
-
-int sum = Sum(arr);
-
-Console.Write($"Cумма элементов, стоящих на нечётных позициях = {sum}");
+int size = AmountNumbers(number);
+int sum = Sum(number, size);
+Console.WriteLine($"Сумму цифр в числе = {sum}");
