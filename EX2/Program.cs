c430714274181f7132b5c88cb9524aa4852f8c5f
LEN4 <fooggreitan@gmail.com>
2022-11-04 23:46:47 +0300

    _1

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,35 +1,31 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
+﻿// Задача 43: Напишите программу, которая 
+// 1. найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// 2. значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+double b1, k1, b2, k2;
 
-int AmountNumbers(int num)
+Console.Write("Введите число b1: ");
+b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число k1: ");
+k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число b2: ");
+b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите число k2: ");
+k2 = Convert.ToDouble(Console.ReadLine());
+
+double meaningX = default;
+double meaningY = default;
+
+void TochcaPer(double x, double y)
 {
-    int index = 0;
-    while (num > 0)
-    {
-        num /= 10; 
-        index++; 
-    }
-    return index;
+    x = (-b2 + b1)/(-k1 + k2);
+    y = k2 * x + b2;
+
+    Console.WriteLine($"({x};{y})");
 }
 
-int Sum(int num, int size)
-{
-    int sum = 0;
-
-    for (int i = 1; i <= size; i++)
-    {
-        sum += num % 10;
-        num /= 10;
-    }
-
-    return sum;
-}
-
-int size = AmountNumbers(number);
-int sum = Sum(number, size);
-Console.WriteLine($"Сумму цифр в числе = {sum}");
+TochcaPer(meaningX, meaningY);
